44b6dd7283d33d8d3143c8d0e667b0ebacbcb760
aduggineni <aduggineni@cyanvave.com>
2025-10-25 12:55:57 +0530

    added multiple-profiles for all services

--- student-service/src/main/java/com/hostelgrid/studentservice/config/SecurityConfig.java
+++ student-service/src/main/java/com/hostelgrid/studentservice/config/SecurityConfig.java
@@ -8,10 +8,45 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+        /**
+     * OpenAPI configuration.
+     * Sets up API info, server, and security schemes for JWT Bearer authentication.
+     * Applies security globally to all endpoints.
+     * Configures OpenAPI documentation.
+     * To access the Swagger UI, visit http://localhost:8083/swagger-ui/index.html
+     */
+    @Bean
+    public OpenAPI v1OpenAPI() {
+        // Define security scheme name
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Student Service API")
+                        .version("v1")
+                        .description("Version 1 of Student Service API"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Student Service API v1"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // Apply globally
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,6 +54,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/actuator/**", "/graphql", "/graphiql/**").permitAll()
+                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().permitAll()
             );
         return http.build();
